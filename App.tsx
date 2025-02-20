99de175708351c32adf1940f23db35439f37f78d
Mike Kazantsev <mk.fraggod@gmail.com>
2025-02-20 20:28:03 +0500

    Implemented outputs

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
-import React, {useState} from 'react';
-import {styled} from 'styled-components';
+import {useState} from 'react'
+import {Share, ToastAndroid} from 'react-native'
+import styled from 'styled-components/native'
+import Icon from 'react-native-vector-icons/Ionicons'
+import Clipboard from '@react-native-clipboard/clipboard'
 
 
 const WAppView = styled.View`
@@ -8,54 +11,92 @@ const WAppView = styled.View`
   padding-horizontal: 10px;
   padding-vertical: 10px;`
 
-const WInput = styled.TextInput`
-  flex: 1;
-  padding: 5px;
-  border: 1px #ddd solid;`
-
 const WBtnRow = styled.View`
   padding: 10px;
   flex-direction: row;
-  justify-content: center;`
+  justify-content: space-between;`
 
 const WBtnBox = styled.TouchableOpacity`
   elevation: 8;
-  margin-horizontal: 10px;
+  flex-direction: row;
+  margin-horizontal: 5px;
   border-radius: 10px;
   padding-vertical: 10px;
   padding-horizontal: 12px;
-  background-color: #555;`
+  background-color: #333;`
+
+const WBtnIcon = styled(Icon)`
+  color: #fff;
+  margin-right: 5;
+  fontSize: 18;`
 
 const WBtnText = styled.Text`
-  font-size: 18px;
   color: #fff;
+  font-size: 14px;
   font-weight: bold;
-  align-self: center;
-  text-transform: uppercase;`
+  align-self: center;`
 
-const WBtn = ({title}) => (
+const WInput = styled.TextInput`
+  flex: 1;
+  selectTextOnFocus: true;
+  textAlignVertical: top;
+  padding: 5px;
+  border: 1px #ddd solid;
+  placeholder-text-color: #999;`
+
+const WBtn = ({icon, title, cb}) => (
   <WBtnBox>
-    <WBtnText>{title}</WBtnText>
+    <WBtnIcon name={icon} />
+    <WBtnText onPress={cb}>{title}</WBtnText>
   </WBtnBox> )
 
 
 const App = () => {
   const [json, json_set] = useState('')
+
+  const json_get_text = () => {
+    try {
+      let notes = JSON.parse(json)
+      if (!Array.isArray(notes)) throw Error('Не экспорт списка Заметок')
+      // notes.sort((a, b) => (a.id+1 || 9999) - (b.id+1 || 9999))
+      return notes.map(n => [
+        n.title ? `-- ${n.title}` : null,
+        n.value ].filter(d => d).join('\n')).join('\n\n')
+    } catch (err) { run_alert(`Ошибка JSON-данных:\n${err.message}`) } }
+
+  // const run_alert = msg => alert(msg)
+  const run_alert = msg =>
+    ToastAndroid.showWithGravity(msg, ToastAndroid.SHORT, ToastAndroid.TOP)
+
+  const run_share = async () => {
+    const text = json_get_text(); if (!text) return
+    try {
+      const res = await Share.share({message: text})
+      if (res.action === Share.dismissedAction) run_alert('Отправка отменена')
+    } catch (err) { run_alert(`Ошибка отправки:\n${err.message}`) } }
+
+  const run_copy = () => {
+    const text = json_get_text(); if (!text) return
+    Clipboard.setString(text)
+    const [n_chars, n_words, n_lines] =
+      [text.length, text.trim().split(/\s+/).length, text.split('\n').length]
+        .map(n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','))
+    run_alert( `Скопировано текстом\n[ ${n_lines}`+
+      ` строк(и), ${n_words} слов, ${n_chars} букв ]` ) }
+
   return (
     <WAppView>
+    <WBtnRow>
+      <WBtn icon='share-social-outline' title='Отправить' cb={run_share} />
+      <WBtn icon='copy-outline' title='Скопировать' cb={run_copy} />
+    </WBtnRow>
     <WInput
       multiline
-      //autoFocus={true}
-      placeholder='Paste JSON here'
-      placeholderTextColor='#999'
+      autoFocus={true}
+      placeholder='Скопировать/вставить JSON сюда'
       onChangeText={text => json_set(text)}
-      value={json}
-      />
-      <WBtnRow>
-        <WBtn title='Share' /> <WBtn title='Copy' />
-      </WBtnRow>
-    </WAppView>
-  )
+      value={json} />
+    </WAppView> )
 }
 
 export default App
